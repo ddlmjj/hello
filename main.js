dd1d09d257419eda9ba8e2e6f6518fccbcf390ba
ddlmjj <68847599+ddlmjj@users.noreply.github.com>
2021-01-19 21:56:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -837,7 +837,7 @@ if(commande === `${prefix}serveur`) {
  }
 //______________________________________________________________________________________________________________________________________________
 
-if(commande === '/vote)') {
+if(commande === `${prefix}vote`) {
 if(!args[0]) return message.channel.send("preciser un vote")
  if(!args[1]) return message.channel.send("preciser un vote") 
 const embedvote = new Discord.MessageEmbed()
